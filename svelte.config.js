8a8963989863dc6d6177ac75007d89a9827b108c
IsGabriellaCurious <gabriellakaitlyn@outlook.com>
2022-10-13 15:53:54 +0100

    Deploy on vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-vercel'
 import svpp from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			edge: false
+		}),
 		files: {
 			lib: "src/lib",
 			assets: "src/assets"
