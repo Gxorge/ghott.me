7ef9f873dd9cde4e27ae10090391ea89dac75a5a
Gxorge <github@ghott.me>
2023-04-29 16:32:50 +0100

    Stopped projects from opening in a new window

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -10,37 +10,37 @@
 <section class="gweb-text-layout gweb-link-grey">
     <div class="content">
 
-        <h3><b>/</b> <a href="projects/theherobrine" target="_blank" rel="noopener">The Herobrine!</a> - 2023</h3>
+        <h3><b>/</b> <a href="projects/theherobrine">The Herobrine!</a> - 2023</h3>
         <p>
             The Herobrine! is my remake of an old Hive MC game-mode that was removed in their Java 2 update.
         </p>
 
-        <h3><b>/</b> <a href="projects/bubbletill" target="_blank" rel="noopener">Bubbletill</a> - 2023</h3>
+        <h3><b>/</b> <a href="projects/bubbletill">Bubbletill</a> - 2023</h3>
         <p>
             Bubbletill is a Point of Sale solution powered by Java at the front end and Python at the backend. Why did I make this? I don't know. Its not finished and probably never will be.
         </p>
 
-        <h3><b>/</b> <a href="/projects/surya" target="_blank" rel="noopener">Surya Real Estate</a> - 2022</h3>
+        <h3><b>/</b> <a href="/projects/surya">Surya Real Estate</a> - 2022</h3>
         <p>
             Part of my BTEC course work for Unit 28. The brief required me to create a website for a local real estate agency called Surya. My work was graded distinction.
         </p>
 
-        <h3><b>/</b> <a href="/projects/isborisgone" target="_blank" rel="noopener">Is Boris Gone?</a> - 2021</h3>
+        <h3><b>/</b> <a href="/projects/isborisgone">Is Boris Gone?</a> - 2021</h3>
         <p>
-            A website created with <a href="https://thomasr.me" target="_blank" rel="noopener">Thomas</a> and <a href="https://lewisakura.moe" target="_blank" rel="noopener">Lewis</a> to know when the UK's former Prime Minister, Boris Johnson, has left No 10.
+            A website created with <a href="https://thomasr.me">Thomas</a> and <a href="https://lewisakura.moe">Lewis</a> to know when the UK's former Prime Minister, Boris Johnson, has left No 10.
         </p>
 
-        <h3><b>/</b> <a href="https://solihull.ghott.me" target="_blank" rel="noopener">Solihull College Coursework</a> - 2021-2023</h3>
+        <h3><b>/</b> <a href="https://solihull.ghott.me">Solihull College Coursework</a> - 2021-2023</h3>
         <p>
             All the coursework for my BTEC Level 3 Extended Diploma and any other random code can be found on the linked GitHub page.
         </p>
 
-        <h3><b>/</b> <a href="/projects/uhccenter" target="_blank" rel="noopener">UHC Center</a> - 2020</h3>
+        <h3><b>/</b> <a href="/projects/uhccenter">UHC Center</a> - 2020</h3>
         <p>
-            A Minecraft UHC network created with <a href="https://lewisakura.moe" target="_blank" rel="noopener">Lewis</a>, built completely from in-house.
+            A Minecraft UHC network created with <a href="https://lewisakura.moe">Lewis</a>, built completely from in-house.
         </p>
 
-        <h3><b>/</b> <a href="projects/aoc" target="_blank" rel="noopener">Advent of Code Solutions</a> - 2015-2022</h3>
+        <h3><b>/</b> <a href="projects/aoc">Advent of Code Solutions</a> - 2015-2022</h3>
         <p>
             All my solutions for Advent of Code 2015-2022. Not all years are finished, but are you surprised?
         </p>
