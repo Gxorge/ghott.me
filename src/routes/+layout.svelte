f545f5b540e87fb4227c44d38353040b62497e2d
Gxorge <github@ghott.me>
2023-05-02 15:13:26 +0100

    wrong src link in footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
     <div class="content gweb-text-layout footer-text has-text-centered">
         <p style="font-size: 15px;"><b>© George Hotten 2023</b></p>
         <p style="font-size: 15px;" class="gweb-link-white">
-            <a href="https://github.com/gxorge/gweb3" target="_blank" rel="noopener">source</a> &middot; 
+            <a href="https://github.com/gxorge/ghott.me" target="_blank" rel="noopener">source</a> &middot; 
             <a href="https://wiki.hotten.cloud" target="_blank" rel="noopener">homelab</a> &middot; 
             <a href="https://github.com/gxorge/" target="_blank" rel="noopener">github</a> &middot; 
             <a href="https://hotten.uk" target="_blank" rel="noopener">hotten.uk</a>
