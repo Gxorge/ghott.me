23708b2c6d74d88c1293af72bea46a24804ea39a
George Hotten <github@ghott.me>
2024-09-21 17:10:41 +0100

    migration to coolify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-static'
 import svpp from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
